a0342319032f4dd811f56fdc7c29fafdf74e0d4d
Zhou Yong <zhouyongwinner@163.com>
2016-04-21 00:03:48 +0800

    co vs compise(next and master branch)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 var Koa = require('koa');
 var app = new Koa();
 
+var compose = require('koa-compose');
+
 /*app.use(async function responseTime(ctx, next){
 	var start = new Date();
 	await next();
@@ -41,41 +43,108 @@ app.use(async function body(ctx, next){
 
 //test
 
+var co = require('co');
+
+function compose1(middleware){
+  return function *(next){
+    if (!next) next = noop();
+
+    var i = middleware.length;
+
+    while (i--) {
+      next = middleware[i].call(this, next);
+    }
+
+    return yield *next;
+  }
+}
+
+/**
+ * Noop.
+ *
+ * @api private
+ */
+
+function *noop(){}
+
+
+
+function* foo(next){
+	console.log('foo0 in');
+	return  yield* next;
+	// console.log('foo0 out');
+	// return temp;
+}
+
 function* foo1(next){
-
-	console.log('====>1');
-	yield next;
-	console.log('1<====');
+	console.log('foo1 in');
+	return yield next;
+	// console.log('foo1 out');
+	// return temp+'11';
 }
 
-function* foo2(next){
-
-	console.log('====>1');
-	yield next;
-	console.log('1<====');
+function* foo2(){
+	console.log('foo2 in');
+	yield Promise.resolve(1);
+	// console.log('foo2 out');
+	// return temp;
 }
 
-function* foo3(next){
 
-	console.log('====>1');
-	yield next;
-	console.log('1<====');
-}
+// var generator = compose1([foo,foo1,foo2]);
+var generator = foo(foo1(foo2()));
 
-var ctx={};
-function* all(){
-	console.log('======>all');
-	yield foo1(foo2(foo3()));
-	console.log('all<======');
-}
 
-var generator = all();
+console.log(generator.next());
+// console.log(generator.next());
+// console.log(generator.next());
 
-console.log( generator.next());
-console.log( generator.next());
-console.log( generator.next());
-console.log( generator.next());
-console.log( generator.next());
+
+// function* g0() {
+// 	console.log('0 in..');
+//   yield 3;
+//   console.log('0 out..');
+// }
+
+
+// function* g1() {
+// 	console.log('1 in..');
+//   yield 2;
+//   yield* g0();
+//   yield 4;
+//   console.log('1 out..');
+// }
+
+// function* g2() {
+// 	console.log('2 in..');
+//   yield 1;
+//   yield* g1();
+//   yield 5;
+//   console.log('2 out..');
+// }
+
+// var iterator = g2();
+
+// console.log(iterator.next()); // { value: 1, done: false }
+// console.log(iterator.next()); // { value: 2, done: false }
+// console.log(iterator.next()); // { value: 3, done: false }
+// console.log(iterator.next()); // { value: 4, done: false }
+// console.log(iterator.next()); // { value: 5, done: false }
+// console.log(iterator.next()); // { value: undefined, done: true }
+
+
+
+
+
+
+
+
+
+// co(aa()).then(function(value){
+// console.log(arguments);
+// }).catch(function(err){
+// 	console.log(err);
+// });
 
 
 
