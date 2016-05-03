70836647c975b3477e963dec7c5ceb3e0357749e
Zhou Yong <zhouyongwinner@163.com>
2016-05-03 15:04:49 +0800

    stash for brew update.

--- src/router.js
+++ src/router.js
@@ -1,22 +1,38 @@
 var router = require('koa-router')();
 
+//video list
+var videos = [
+	{
+		id: 10001,
+		img:'http://r4.ykimg.com/0541010157260AB4641DA419FA98C5D4',
+		title: '起小点TOP10 VOL192 老树盘根！亚洲捆绑教你扭曲战场起小点TOP10 VOL192 老树盘根！亚洲捆绑教你扭曲战场起小点TOP10 VOL192 老树盘根！亚洲捆绑教你扭曲战场起小点TOP10 VOL192 老树盘根！亚洲捆绑教你扭曲战场',
+		voteCount: 1023,
+		publishDate: '1周前'	
+	},
+	{
+		id: 10001,
+		img:'http://r4.ykimg.com/0541010157260AB4641DA419FA98C5D4',
+		title: '起小点TOP10 VOL192 老树盘根！亚洲捆绑教你扭曲战场',
+		voteCount: 10,
+		publishDate: '1周前'	
+	}
+];
 
-var user = {
-		name: {
-			first: 'Zhou',
-			last: 'Yong'
-		},
-		desc: '<h2>here is variable.<h2>',
-		age: '<b>23</b>'
-	};
+var video ={
+	id: 10001,
+	src:'http://player.youku.com/player.php/sid/XMTU0NTgyMjc0OA==/v.swf',
+	title: '起小点TOP10 VOL192 老树盘根！亚洲捆绑教你扭曲战场',
+	voteCount: 10,
+	publishDate: '1周前'	
+}
 
 router.get('/', async (ctx, next) =>{
-	await ctx.render('index.jade', {user: user});
+	await ctx.render('index.jade', {videos: videos});
 });
 
 router.get('/video/:id', async (ctx, next) =>{
 	
-	await ctx.render('video.jade', {user: user});
+	await ctx.render('video.jade', {video: video});
 });
 
 module.exports= router;
